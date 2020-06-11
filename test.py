3a4cdff83aad6d74a6091341048eb81150838ec1
Chen Lei <chillychen1991@gmail.com>
2020-06-12 01:35:19 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-print('hello world')
-pass
-assert (1, 2, 3) == (1, 2, 3)
+def test():
+    print('hello world')
+    pass
+    assert (1, 2, 3) == (1, 2, 3)
